39bebd607257c851a776e2cfbea453d487411861
LI ZEYAN <zy-li14@hotmail.com>
2016-10-26 09:31:22 +0800

    replace im2col

--- nnpool.m
+++ nnpool.m
@@ -14,7 +14,12 @@ function output = nnpool(input, kernel_size, pad)
     input_shape (pad + 1:pad + h, pad + 1:pad + w, :, :) = input;
     for n_cnt = 1:n
         for c_cnt = 1:c
-            col = im2col (input_shape(:, :, c_cnt, n_cnt), [kernel_size, kernel_size], 'distinct');
+%             tic
+%             col = im2col (input_shape(:, :, c_cnt, n_cnt), [kernel_size, kernel_size], 'distinct');
+%             toc
+%             tic
+            col = im2col_distinct (input_shape(:, :, c_cnt, n_cnt), [kernel_size, kernel_size]);
+%             toc
             col = mean (col);
 %             disp (size(col));
             output (:, :, c_cnt, n_cnt) = col2im (col, [1, 1], [hn / kernel_size, wn / kernel_size], 'distinct');
@@ -24,5 +29,27 @@ function output = nnpool(input, kernel_size, pad)
 %     disp (size(input));
 %     disp (size(input_shape));
 %     disp (size(output));
+end
+
+function out = im2col_distinct(A,blocksize)
+
+    nrows = blocksize(1);
+    ncols = blocksize(2);
+    nele = nrows*ncols;
+
+    row_ext = mod(size(A,1),nrows);
+    col_ext = mod(size(A,2),ncols);
+
+    padrowlen = (row_ext~=0)*(nrows - row_ext);
+    padcollen = (col_ext~=0)*(ncols - col_ext);
+
+    A1 = zeros(size(A,1)+padrowlen,size(A,2)+padcollen);
+    A1(1:size(A,1),1:size(A,2)) = A;
+
+    t1 = reshape(A1,nrows,size(A1,1)/nrows,[]);
+    t2 = reshape(permute(t1,[1 3 2]),size(t1,1)*size(t1,3),[]);
+    t3 =  permute(reshape(t2,nele,size(t2,1)/nele,[]),[1 3 2]);
+    out = reshape(t3,nele,[]);
+
 end
             
--- nnpool_bp.m
+++ nnpool_bp.m
@@ -8,10 +8,33 @@ function down_delta = nnpool_bp(input, delta, kernel_size, pad)
     down_delta = zeros(h, w, c, n);
     for n_cnt = 1:n
         for c_cnt = 1:c
-            col = im2col (delta(:, :, c_cnt, n_cnt), [1, 1], 'distinct');
+            col = im2col_distinct (delta(:, :, c_cnt, n_cnt), [1, 1]);
             col = repmat (col, [kernel_size * kernel_size, 1]);
             im = col2im (col, [kernel_size, kernel_size], [h + pad * 2, w + pad * 2], 'distinct');
             down_delta(:, :, c_cnt, n_cnt) = im (pad + 1: pad + h, pad + 1: pad + w);
         end
     end
 end
+
+function out = im2col_distinct(A,blocksize)
+
+    nrows = blocksize(1);
+    ncols = blocksize(2);
+    nele = nrows*ncols;
+
+    row_ext = mod(size(A,1),nrows);
+    col_ext = mod(size(A,2),ncols);
+
+    padrowlen = (row_ext~=0)*(nrows - row_ext);
+    padcollen = (col_ext~=0)*(ncols - col_ext);
+
+    A1 = zeros(size(A,1)+padrowlen,size(A,2)+padcollen);
+    A1(1:size(A,1),1:size(A,2)) = A;
+
+    t1 = reshape(A1,nrows,size(A1,1)/nrows,[]);
+    t2 = reshape(permute(t1,[1 3 2]),size(t1,1)*size(t1,3),[]);
+    t3 =  permute(reshape(t2,nele,size(t2,1)/nele,[]),[1 3 2]);
+    out = reshape(t3,nele,[]);
+
+end
+      
